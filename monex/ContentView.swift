20c23fe2cad41668f2ca1492bc056307c6b0bc9e
Batuhan Gözegü <batuhangozegu@gmail.com>
2025-08-07 15:11:17 +0300

    Initial Commit

---
+++ monex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  monex
+//
+//  Created by Batuhan Gözegü on 7.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ monex/monexApp.swift
@@ -0,0 +1,17 @@
+//
+//  monexApp.swift
+//  monex
+//
+//  Created by Batuhan Gözegü on 7.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct monexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

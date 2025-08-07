08faa57c4850d22b46acebfe4c6eb68d57159350
Batuhan Gözegü <batuhangozegu@gmail.com>
2025-08-07 17:56:28 +0300

    add Typography config and missing font file

---
+++ monex/Resources/Typography.swift
@@ -0,0 +1,26 @@
+//
+//  Typography.swift
+//  monex
+//
+//  Created by Batuhan Gözegü on 7.08.2025.
+//
+import SwiftUI
+import Foundation
+
+enum Typography {
+    
+    static let heading36 = Font.custom("Inter-Medium", size: 36)
+    static let heading24 = Font.custom("Inter-Medium", size: 24)
+    
+    static let subheading20 = Font.custom("Inter-Medium", size: 24)
+    static let subheading16 = Font.custom("Inter-SemiBold", size: 16)
+    static let subheading12 = Font.custom("Inter-SemiBold", size: 12)
+    
+    static let body16 = Font.custom("Inter-Regular", size: 16)
+    static let body14 = Font.custom("Inter-Regular", size: 14)
+    static let body12 = Font.custom("Inter-Regular", size: 12)
+    
+    static let small10 = Font.custom("Inter-Regular", size: 10)
+    
+    
+}
